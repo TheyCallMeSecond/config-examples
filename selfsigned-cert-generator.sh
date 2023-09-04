34d61c1bbd820980f2d6deed6adf9fe14cac9891
TheyCallMeSecond <73380906+TheyCallMeSecond@users.noreply.github.com>
2023-09-04 12:11:39 +0330

    Update selfsigned-cert-generator.sh

--- selfsigned-cert-generator.sh
+++ selfsigned-cert-generator.sh
@@ -4,7 +4,7 @@ openssl genrsa -out ca.key 2048
 
 openssl req -new -x509 -days 3650 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Apple, Inc./CN=Apple Root CA" -out ca.crt
 
-openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Apple, Inc./CN=*.Apple.com" -out server.csr
+openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Apple, Inc./CN=*.apple.com" -out server.csr
 
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:Apple.com,DNS:www.Apple.com") -days 3650 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:apple.com,DNS:www.apple.com") -days 3650 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 
