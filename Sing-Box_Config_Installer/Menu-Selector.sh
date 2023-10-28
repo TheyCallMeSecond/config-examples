5e4a33bb92e3d42c0d697e3f2c426fd7583b1036
TheyCallMeSecond <73380906+TheyCallMeSecond@users.noreply.github.com>
2023-10-28 15:47:49 +0330

    Update Menu-Selector.sh

--- Sing-Box_Config_Installer/Menu-Selector.sh
+++ Sing-Box_Config_Installer/Menu-Selector.sh
@@ -42,6 +42,33 @@ while true; do
 
     echo
 
+    # Color
+    GREEN="\e[32m"
+    RESET="\e[0m"
+
+    # Call the functions to collect system information
+    check_system_info
+    check_system_ip
+
+    # List of processes to check, along with their custom names
+    processes=("SH:Hysteria2" "TS:TUIC" "sing-box:Reality" "ST:ShadowTLS" "SBW:WARP")
+
+    # Display the collected system information
+    echo "#######################################################"    
+    echo "${GREEN}Operating System:${RESET} $SYS"
+    echo "${GREEN}Kernel:${RESET} $KERNEL"
+    echo "${GREEN}Architecture:${RESET} $ARCHITECTURE"
+    echo "${GREEN}Virtualization:${RESET} $VIRT"
+    echo "${GREEN}IPv4:${RESET} $WAN4 $COUNTRY4"
+    echo "${GREEN}IPv6:${RESET} $WAN6 $COUNTRY6"
+    echo "======================================================="
+    for process_info in "${processes[@]}"; do
+        IFS=":" read -r process_name custom_name <<<"$process_info"
+        check_and_display_process_status "$process_name" "$custom_name"
+    done
+    echo "======================================================="
+    echo "#######################################################" 
+    echo
     echo -e "1:  \e[93mTUI Menu\e[0m"
     echo -e "2:  \e[93mLegacy Menu\e[0m"
     echo -e "0:  \e[95mExit\e[0m"
@@ -54,22 +81,22 @@ while true; do
         sudo apt update
         sudo apt install dialog qrencode jq openssl python3 python3-pip -y
         pip install httpx requests --break-system-packages
-        
+
         clear
         tui
         ;;
     2)
-        sudo apt update    
-        sudo apt install dialog qrencode jq openssl python3 python3-pip -y  
+        sudo apt update
+        sudo apt install dialog qrencode jq openssl python3 python3-pip -y
         pip install httpx requests --break-system-packages
-        
+
         clear
-        legacy      
+        legacy
         ;;
     0)
-        clear   
+        clear
         echo "Exiting."
-        exit 0 
+        exit 0
         ;;
     *)
         echo "Invalid choice. Please select a valid option."
