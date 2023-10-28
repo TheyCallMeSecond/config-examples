b335ee831c1a73993dd5fd14815d3b2670c52a5e
TheyCallMeSecond <73380906+TheyCallMeSecond@users.noreply.github.com>
2023-10-28 15:47:26 +0330

    Update Source.sh

--- Sing-Box_Config_Installer/Source.sh
+++ Sing-Box_Config_Installer/Source.sh
@@ -1653,3 +1653,69 @@ optimize_server() {
     clear
 
 }
+
+check_system_info() {
+    if [ $(type -p systemd-detect-virt) ]; then
+        VIRT=$(systemd-detect-virt)
+    elif [ $(type -p hostnamectl) ]; then
+        VIRT=$(hostnamectl | awk '/Virtualization/{print $NF}')
+    elif [ $(type -p virt-what) ]; then
+        VIRT=$(virt-what)
+    fi
+
+    [ -s /etc/os-release ] && SYS="$(grep -i pretty_name /etc/os-release | cut -d \" -f2)"
+    [[ -z "$SYS" && $(type -p hostnamectl) ]] && SYS="$(hostnamectl | grep -i system | cut -d : -f2)"
+    [[ -z "$SYS" && $(type -p lsb_release) ]] && SYS="$(lsb_release -sd)"
+    [[ -z "$SYS" && -s /etc/lsb-release ]] && SYS="$(grep -i description /etc/lsb-release | cut -d \" -f2)"
+    [[ -z "$SYS" && -s /etc/redhat-release ]] && SYS="$(grep . /etc/redhat-release)"
+    [[ -z "$SYS" && -s /etc/issue ]] && SYS="$(grep . /etc/issue | cut -d '\' -f1 | sed '/^[ ]*$/d')"
+
+    REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "amazon linux" "arch linux" "alpine")
+    RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Arch" "Alpine")
+    EXCLUDE=("")
+    MAJOR=("9" "16" "7" "7" "" "")
+    PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update" "pacman -Sy" "apk update -f")
+    PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "pacman -S --noconfirm" "apk add --no-cache")
+    PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove" "pacman -Rcnsu --noconfirm" "apk del -f")
+
+    for int in "${!REGEX[@]}"; do [[ $(tr 'A-Z' 'a-z' <<<"$SYS") =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && break; done
+    [ -z "$SYSTEM" ] && error " $(text 5) "
+
+    # First exclude specific systems included in EXCLUDE. Other systems need to be compared with major releases.
+    for ex in "${EXCLUDE[@]}"; do [[ ! $(tr 'A-Z' 'a-z' <<<"$SYS") =~ $ex ]]; done &&
+        [[ "$(echo "$SYS" | sed "s/[^0-9.]//g" | cut -d. -f1)" -lt "${MAJOR[int]}" ]] && error " $(text_eval 6) "
+
+    KERNEL=$(uname -r)
+    ARCHITECTURE=$(uname -m)
+}
+
+check_system_ip() {
+    IP4=$(wget -4 -qO- --no-check-certificate --user-agent=Mozilla --tries=2 --timeout=1 http://ip-api.com/json/) &&
+        WAN4=$(expr "$IP4" : '.*query\":[ ]*\"\([^"]*\).*') &&
+        COUNTRY4=$(expr "$IP4" : '.*country\":[ ]*\"\([^"]*\).*') &&
+        ASNORG4=$(expr "$IP4" : '.*isp\":[ ]*\"\([^"]*\).*') &&
+        [[ "$L" = C && -n "$COUNTRY4" ]] && COUNTRY4=$(translate "$COUNTRY4")
+
+    IP6=$(wget -6 -qO- --no-check-certificate --user-agent=Mozilla --tries=2 --timeout=1 https://api.ip.sb/geoip) &&
+        WAN6=$(expr "$IP6" : '.*ip\":[ ]*\"\([^"]*\).*') &&
+        COUNTRY6=$(expr "$IP6" : '.*country\":[ ]*\"\([^"]*\).*') &&
+        ASNORG6=$(expr "$IP6" : '.*isp\":[ ]*\"\([^"]*\).*') &&
+        [[ "$L" = C && -n "$COUNTRY6" ]] && COUNTRY6=$(translate "$COUNTRY6")
+
+}
+
+check_and_display_process_status() {
+    PROCESS_NAME="$1"
+    CUSTOM_NAME="$2"
+    if pgrep -x "$PROCESS_NAME" >/dev/null; then
+        PID=$(pgrep -x "$PROCESS_NAME")
+        PORT=$(lsof -t -i :0 | xargs -I {} sh -c 'echo -n {}')
+        if [ -n "$PORT" ]; then
+            echo "$CUSTOM_NAME: open (PID: $PID, Port: $PORT)"
+        else
+            echo "$CUSTOM_NAME: open (PID: $PID, Port: N/A)"
+        fi
+    else
+        echo "$CUSTOM_NAME: closed"
+    fi
+}
