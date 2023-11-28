b158456a00f1c179c90525e7ca1951fa3afbbe99
TheyCallMeSecond <73380906+TheyCallMeSecond@users.noreply.github.com>
2023-11-28 20:47:06 +0330

    Update Menu-Selector.sh

--- Sing-Box_Config_Installer/Menu-Selector.sh
+++ Sing-Box_Config_Installer/Menu-Selector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-    echo \e[91m"This script requires root privileges. Please run it as root"\e[0m
+    echo "\e[91mThis script requires root privileges. Please run it as root\e[0m"
     exit 1
 fi
 
